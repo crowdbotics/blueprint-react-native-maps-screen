39c4efb0411355b83fa7f986d380da80ea36a38e
tanduong <tanduong@live.com>
2020-02-13 07:04:32 -0600

    PLAT 1223

--- install.sh
+++ install.sh
@@ -33,7 +33,7 @@ EXT_POINT_1="@BlueprintInsertion"
 EXT_POINT_2="@BlueprintImportInsertion"
 EXT_POINT_3="@BlueprintNavigationInsertion"
 DATA_1="{ name: '${NAME}', human_name: '${NAME}', access_route: '${NAME}', icon: 'map'},"
-DATA_2="import { ${NAME}Navigator } from '..\/features\/${NAME}\/navigator';"
+DATA_2="import ${NAME}Navigator from '..\/features\/${NAME}\/navigator';"
 DATA_3="${NAME}: { screen: ${NAME}Navigator },"
 
 echo "create blueprint folder"
